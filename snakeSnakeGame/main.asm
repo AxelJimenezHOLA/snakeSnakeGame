3f62a798596599b7668918f0c568bdb464a799da
AxelJimenezHOLA <axel.jimenez85@uabc.edu.mx>
2024-11-11 20:00:45 -0800

    Agregar archivos de proyecto.

---
+++ snakeSnakeGame/main.asm
@@ -0,0 +1,13 @@
+.386
+.model flat, stdcall
+.stack 4096
+
+.DATA
+	; Define your variables here
+.CODE
+main PROC
+	; Your main procedure starts here
+	RET ; Return from the procedure
+main ENDP
+
+END main
